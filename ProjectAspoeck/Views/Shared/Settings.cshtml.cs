9a1ea5d9e93ddd9a3f94d87470f14d2adeda5b4f
offsimon <lugers@190089@sus.htl-grieskirchen.at>
2023-03-07 11:29:35 +0100

    Julian html seiten eingebunden

---
+++ ProjectAspoeck/Views/Shared/Settings.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JausenbestellungAspoeck.Pages
+{
+    public class Home_PageModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+        
+    }
+}
