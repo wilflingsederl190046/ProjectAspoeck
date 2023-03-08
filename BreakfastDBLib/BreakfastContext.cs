30047ad29362700c70bc3f1060a0ed4710c87dfc
wilflingsederl190046 <wilflingsederl190046@sus.htl-grieskirchen.at>
2023-03-08 09:39:21 +0100

    Minior project structure changes

--- BreakfastDBLib/BreakfastContext.cs
+++ BreakfastDBLib/BreakfastContext.cs
@@ -25,10 +25,18 @@ public partial class BreakfastContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(LocalDB)\\mssqllocaldb;attachdbfilename=C:\\Users\\lukas\\OneDrive\\Desktop\\SYP\\Aspoeck_project\\Datenbank\\BreakfastDB.mdf;integrated security=True;MultipleActiveResultSets=True");
-  //=> optionsBuilder.UseSqlServer("Server=(LocalDB)\\mssqllocaldb;attachdbfilename=C:\\4_Klasse\\PRASPÖCK\\Project\\Project\\ProjectAspoeck\\BreakfastDBLib\\BreakfastDB.mdf;integrated security=True;MultipleActiveResultSets=True");
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    optionsBuilder.UseSqlServer("Server=(LocalDB)\\mssqllocaldb;attachdbfilename=C:\\Users\\lukas\\OneDrive\\Desktop\\SYP\\Aspoeck_project\\Datenbank\\BreakfastDB.mdf;integrated security=True;MultipleActiveResultSets=True;TrustServerCertificate=True", builder =>
+    {
+      builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(1), null);
+    });
+    //=> optionsBuilder.UseSqlServer("Server=(LocalDB)\\mssqllocaldb;attachdbfilename=C:\\4_Klasse\\PRASPÖCK\\Project\\Project\\ProjectAspoeck\\BreakfastDBLib\\BreakfastDB.mdf;integrated security=True;MultipleActiveResultSets=True");
+
+    base.OnConfiguring(optionsBuilder);
+  }
+
+
 
   protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- ProjectAspoeck/Controllers/HomeController.cs
+++ ProjectAspoeck/Controllers/HomeController.cs
@@ -8,82 +8,78 @@ using ProjectAspoeck.Models;
 
 using System.Diagnostics;
 
-namespace ProjectAspoeck.Controllers
+namespace ProjectAspoeck.Controllers;
+
+
+public class HomeController : Controller
 {
+  BreakfastContext _db = new BreakfastContext();
+  private readonly ILogger<HomeController> _logger;
+  public HomeController(ILogger<HomeController> logger) => _logger = logger;
 
-  public class HomeController : Controller
+  [HttpGet]
+  public IActionResult Index()
   {
-    BreakfastContext _db = new BreakfastContext();
-    private readonly ILogger<HomeController> _logger;
-    public HomeController(ILogger<HomeController> logger)
+    LoginModel _loginModel = new LoginModel();
+    return View(_loginModel);
+  }
+  [HttpPost]
+  public IActionResult Index(LoginModel _loginModel)
+  {
+
+    List<UserDTO> _users = _db.Users.Select(x => new UserDTO { UserId = x.UserId, LastName = x.LastName, FirstName = x.FirstName }).ToList();
+    Console.WriteLine(_users);
+
+    User? user = _db.Users.Where(m => m.UserName == _loginModel.LoginId && m.ChipNumber == _loginModel.Password).FirstOrDefault();
+    if (user == null)
     {
-      _logger = logger;
+
+      ViewBag.LoginStatus = 0;
     }
-
-    [HttpGet]
-    public IActionResult Index()
+    else
     {
-      LoginModel _loginModel = new LoginModel();
-      return View(_loginModel);
-    }
-    [HttpPost]
-    public IActionResult Index(LoginModel _loginModel)
-    {
-
-      List<UserDTO> _users = _db.Users.Select(x => new UserDTO { UserId = x.UserId, LastName = x.LastName, FirstName = x.FirstName }).ToList();
-      Console.WriteLine(_users);
-
-      User? user = _db.Users.Where(m => m.UserName == _loginModel.LoginId && m.ChipNumber == _loginModel.Password).FirstOrDefault();
-      if (user == null)
+      _loginModel.UserId = user.UserId;
+      var loginModel = new LoginModel
       {
+        UserId = _loginModel.UserId,
+        // add other necessary properties here
+      };
+      return RedirectToAction("Home_Page", "Home", loginModel);
+    }
+    return View(_loginModel);
+  }
 
-        ViewBag.LoginStatus = 0;
+  public IActionResult Home_Page(LoginModel loginModel)
+  {
+
+    User user = _db.Users.Where(x => x.UserId == loginModel.UserId).FirstOrDefault();
+    string name = user.UserName ?? "UserName";
+    var homeModel = new Home_PageModel();
+    homeModel.UserName = name;
+    homeModel.UserId = loginModel.UserId;
+    var orders = new List<OrderViewModel>
+           {
+              new OrderViewModel { OrderNumber = 1, OrderDate = "Mo, 01.01.2023", OrderAmount = 130.00m, IsPaid = true },
+              new OrderViewModel { OrderNumber = 2, OrderDate = "Di, 15.02.2023", OrderAmount = 72.50m, IsPaid = false },
+              new OrderViewModel { OrderNumber = 3, OrderDate = "Mi, 28.02.2023", OrderAmount = 42.00m, IsPaid = false }
+          };
+
+    homeModel.orders = orders;
+    return View(homeModel);
       }
-      else
+      public IActionResult Settings(SettingsModel settingsModel)
       {
-        _loginModel.UserId = user.UserId;
-        var loginModel = new LoginModel
-        {
-          UserId = _loginModel.UserId,
-          // add other necessary properties here
-        };
-        return RedirectToAction("Home_Page", "Home", loginModel);
+          SettingsModel settings= new SettingsModel();
+          return View(settings);
       }
-      return View(_loginModel);
-    }
+      public IActionResult Privacy()
+      {
+       return View();
+       }
 
-    public IActionResult Home_Page(LoginModel loginModel)
-    {
-
-      User user = _db.Users.Where(x => x.UserId == loginModel.UserId).FirstOrDefault();
-      string name = user.UserName ?? "UserName";
-      var homeModel = new Home_PageModel();
-      homeModel.UserName = name;
-      homeModel.UserId = loginModel.UserId;
-      var orders = new List<OrderViewModel>
-             {
-                new OrderViewModel { OrderNumber = 1, OrderDate = "Mo, 01.01.2023", OrderAmount = 130.00m, IsPaid = true },
-                new OrderViewModel { OrderNumber = 2, OrderDate = "Di, 15.02.2023", OrderAmount = 72.50m, IsPaid = false },
-                new OrderViewModel { OrderNumber = 3, OrderDate = "Mi, 28.02.2023", OrderAmount = 42.00m, IsPaid = false }
-            };
-
-      homeModel.orders = orders;
-      return View(homeModel);
-        }
-        public IActionResult Settings(SettingsModel settingsModel)
-        {
-            SettingsModel settings= new SettingsModel();
-            return View(settings);
-        }
-        public IActionResult Privacy()
-        {
-         return View();
-         }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
   }
 }
