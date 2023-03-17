38d7646d7c41545833f4674ef572e70dd09d3013
wilflingsederl190046 <wilflingsederl190046@sus.htl-grieskirchen.at>
2023-03-17 11:55:24 +0100

    Corrected the code in Home_PageModel with name violations and other minior changes

--- ProjectAspoeck/Controllers/EncryptionHelper.cs
+++ ProjectAspoeck/Controllers/EncryptionHelper.cs
@@ -1,73 +1,61 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 using System;
-using System.IO;
-using System.Security.Cryptography;
-using System.Text;
 
-namespace ProjectAspoeck.Controllers
+namespace ProjectAspoeck.Controllers;
+
+
+public static class EncryptionHelper
 {
+  private static readonly byte[] Salt = Encoding.UTF8.GetBytes("InsertSaltHere");
 
-    public static class EncryptionHelper
+  private const int Iterations = 10000;
+  private const int KeySize = 256;
+
+  public static string Encrypt(string plainText, string password)
+  {
+    byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+    byte[] keyBytes = new Rfc2898DeriveBytes(password, Salt, Iterations).GetBytes(KeySize / 8);
+
+    using var aes = Aes.Create();
+    aes.Mode = CipherMode.CBC;
+
+    using var encryptor = aes.CreateEncryptor(keyBytes, aes.IV);
+    using var ms = new MemoryStream();
+    ms.Write(BitConverter.GetBytes(aes.IV.Length), 0, sizeof(int));
+    ms.Write(aes.IV, 0, aes.IV.Length);
+
+    using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
     {
-        private static readonly byte[] Salt = Encoding.UTF8.GetBytes("InsertSaltHere");
-
-        private const int Iterations = 10000;
-        private const int KeySize = 256;
-
-        public static string Encrypt(string plainText, string password)
-        {
-            byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
-            byte[] keyBytes = new Rfc2898DeriveBytes(password, Salt, Iterations).GetBytes(KeySize / 8);
-
-            using (var aes = Aes.Create())
-            {
-                aes.Mode = CipherMode.CBC;
-
-                using (var encryptor = aes.CreateEncryptor(keyBytes, aes.IV))
-                using (var ms = new MemoryStream())
-                {
-                    ms.Write(BitConverter.GetBytes(aes.IV.Length), 0, sizeof(int));
-                    ms.Write(aes.IV, 0, aes.IV.Length);
-
-                    using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
-                    {
-                        cs.Write(plainBytes, 0, plainBytes.Length);
-                        cs.Close();
-                    }
-
-                    return Convert.ToBase64String(ms.ToArray());
-                }
-            }
-        }
-
-        public static string Decrypt(string cipherText, string password)
-        {
-           
-            byte[] cipherBytes = Convert.FromBase64String(cipherText);
-            byte[] keyBytes = new Rfc2898DeriveBytes(password, Salt, Iterations).GetBytes(KeySize / 8);
-
-            using (var aes = Aes.Create())
-            {
-                aes.Mode = CipherMode.CBC;
-
-                int ivLength = BitConverter.ToInt32(cipherBytes, 0);
-                byte[] iv = new byte[ivLength];
-                Array.Copy(cipherBytes, sizeof(int), iv, 0, ivLength);
-
-                using (var decryptor = aes.CreateDecryptor(keyBytes, iv))
-                using (var ms = new MemoryStream())
-                {
-                    using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write))
-                    {
-                        cs.Write(cipherBytes, ivLength + sizeof(int), cipherBytes.Length - ivLength - sizeof(int));
-                        cs.Close();
-                    }
-
-                    return Encoding.UTF8.GetString(ms.ToArray());
-                }
-            }
-            
-        }
+      cs.Write(plainBytes, 0, plainBytes.Length);
+      cs.Close();
     }
+
+    return Convert.ToBase64String(ms.ToArray());
+  }
+
+  public static string Decrypt(string cipherText, string password)
+  {
+
+    byte[] cipherBytes = Convert.FromBase64String(cipherText);
+    byte[] keyBytes = new Rfc2898DeriveBytes(password, Salt, Iterations).GetBytes(KeySize / 8);
+
+    using var aes = Aes.Create();
+    aes.Mode = CipherMode.CBC;
+
+    int ivLength = BitConverter.ToInt32(cipherBytes, 0);
+    byte[] iv = new byte[ivLength];
+    Array.Copy(cipherBytes, sizeof(int), iv, 0, ivLength);
+
+    using var decryptor = aes.CreateDecryptor(keyBytes, iv);
+    using var ms = new MemoryStream();
+    using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write))
+    {
+      cs.Write(cipherBytes, ivLength + sizeof(int), cipherBytes.Length - ivLength - sizeof(int));
+      cs.Close();
+    }
+
+    return Encoding.UTF8.GetString(ms.ToArray());
+
+  }
 }
