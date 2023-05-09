0b536e7e3648a616a7e78b86bb54a620c20dcf87
wilflingsederl190046 <wilflingsederl190046@sus.htl-grieskirchen.at>
2023-05-09 10:18:55 +0200

    fixed the update time that it loads instant after loading the page

--- ProjectAspoeck/Models/Home_PageModel.cs
+++ ProjectAspoeck/Models/Home_PageModel.cs
@@ -6,5 +6,5 @@ public class Home_PageModel
     public string? UserName { get; set; }
     public List<OrderViewModel> Orders { get; set; }
     public string SessionString { get; set; } = null!;
-    public double? MoneyLeftToPay { get; set; }
+    public double MoneyLeftToPay { get; set; }
 }
